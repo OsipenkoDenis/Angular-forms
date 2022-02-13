ec33c5d2a0182b40af57f38c48762152b0a449f8
osipenkoDenis <denis.osipenko@inbox.ru>
2022-02-13 17:31:49 +0300

    to use Ngx Formly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule, FormlyFieldConfig } from '@ngx-formly/core'
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
@@ -10,6 +12,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
+
+export function validateRequired(err: any, field: FormlyFieldConfig): string {
+	return `This field is required.`
+}
 
 @NgModule({
   imports: [
@@ -22,6 +28,12 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    FormlyModule.forRoot({
+			validationMessages: [
+				{ name: 'required', message: validateRequired },
+			],
+		}),
+    FormlyMaterialModule,
   ],
   declarations: [
     AppComponent,
@@ -31,3 +43,7 @@ import {MatButtonModule} from '@angular/material/button';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+function validationMessages(validationMessages: any, arg1: { name: string; message: (err: any, field: FormlyFieldConfig) => string; }[]): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
+  throw new Error('Function not implemented.');
+}
--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { Hero } from '../hero'; 
+import { FormGroup} from '@angular/forms';
+import {FormlyFieldConfig} from '@ngx-formly/core';
 
 @Component({
   selector: 'app-hero-form',
@@ -9,26 +8,51 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
-  form: FormGroup | any;
-  
+  form = new FormGroup({});
+  model = { name: '',
+            alterEgo: '',
+            heroPower: '', };
+  fields: FormlyFieldConfig[] = [
+    { 
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        label: 'Name',
+        placeholder: '',
+        required: true,
+      }
+    },
+    {
+      key: 'alterEgo',
+      type: 'input',
+      templateOptions: {
+        label: 'Alter Ego',
+        placeholder: '',
+        required: false,
+      }
+    },
+    {
+      key: 'heroPower',
+      type: 'select',
+      templateOptions: {
+        label: 'Hero Power',
+        placeholder: '',
+        description: 'Select the Hero Power',
+        required: true,
+        options: [
+          { value: 'Really Smart', label: 'Really Smart'  },
+          { value: 'Super Flexible', label: 'Super Flexible'  },
+          { value: 'Super Hot', label: 'Super Hot'  },
+          { value: 'Weather Changer', label: 'Weather Changer'  },
+          
+        ],
+      },
+    },
+  ];
   ngOnInit() {
-      this.form = new FormGroup({
-        name: new FormControl("", Validators.required),
-        alterEgo: new FormControl(""),
-        heroPower: new FormControl("Really Smart", Validators.required)
-      })
   }
-
-  powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
-  model = new Hero(18, '', this.powers[0], '');
-
-  submitted = false;
+   submitted = false;
 
   onSubmit() { 
     this.submitted = true; }
-
-  newHero() {
-    this.model = new Hero(42, '', '');
   }
-}
